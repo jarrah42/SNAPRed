0a9f43d1416f524e1fd949b5a57ca2dc7140d1ee
Kort Travis <traviska@ornl.gov>
2024-11-08 12:23:49 -0500

    'LiveMetadata' DAO.

---
+++ src/snapred/backend/dao/LiveMetadata.py
@@ -0,0 +1,27 @@
+from typing import ClassVar
+from datetime import datetime
+from pydantic import BaseModel
+
+from snapred.backend.dao.state import DetectorState
+
+
+class LiveMetadata:
+    """Metadata about any in-progress data acquisition."""
+    
+    # Implementation notes:
+    #
+    #   * If there's no run currently active, the `runNumber` will be `INACTIVE_RUN`.
+    #
+    #   * Metadata may change at any time.  For example, the run may terminate and become inactive.
+    #     For this reason, in most cases this DAO should never be cached.
+    #
+    
+    INACTIVE_RUN: ClassVar[int] = 0
+    
+    runNumber: str
+    startTime: datetime
+    
+    detectorState: DetectorState
+ 
+    def runInProgress(self):
+        return int(runNumber) != INACTIVE_RUN
